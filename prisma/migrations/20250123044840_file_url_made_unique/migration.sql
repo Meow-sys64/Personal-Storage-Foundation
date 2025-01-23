75d0ea6e04d1967f455bb37aea55d9314c67ea5e
Meow-sys64 <g6vhytX1Mgipaz@proton.me>
2025-01-22 23:59:07 -0500

    File Url in schema made Unique

---
+++ prisma/migrations/20250123044840_file_url_made_unique/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[url]` on the table `File` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[name]` on the table `Folder` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `updatedAt` to the `File` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Folder` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "File" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Folder" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_url_key" ON "File"("url");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Folder_name_key" ON "Folder"("name");
